c3faae9faddcfd8df7393793ca5e65a164d8f59e
Jehmess <39226383+Jehmess@users.noreply.github.com>
2020-11-26 21:44:17 -0500

    dark mode complete without saved preferences

--- scripts.js
+++ scripts.js
@@ -4,4 +4,10 @@ function deepSea(){
     element.classList.toggle('darkMode');
     let element2 = document.getElementById('body')
     element2.classList.toggle('body2');
+    let element3 = document.getElementById('topHead')
+    element3.classList.toggle('topHead2');
+    let element4 = document.getElementById('footer')
+    element4.classList.toggle('footer2');
+    let element5 = document.getElementById('navBar')
+    element5.classList.toggle("navBar2");
 };
