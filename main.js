6fd27d095b6510aecaf09f4f6a7d54bdd83e66d7
Jenna Beach <beac0045@algonquinlive.com>
2021-01-08 17:17:26 -0500

    to top button // javascript

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+//STICKY NAV BAR
+window.addEventListener('scroll', myFunction);
+var navbar = document.getElementById("navbar"); 
+var sticky = navbar.offsetTop; 
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")   
@@ -16,3 +11,21 @@ function myFunction() {
   } 
 } 
 
+
+// TO PAGE TOP BUTTON
+topbutton = document.getElementById("topBtn"); 
+
+window.addEventListener('scroll', scrollFunction);
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
